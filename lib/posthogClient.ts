f7db90920b7a5618b28806208762d6dadca8f12d
tymyevhenmpw <tymyevhen@gmail.com>
2025-07-22 21:43:01 +0200

    Commit

--- lib/posthogClient.ts
+++ lib/posthogClient.ts
@@ -1,11 +1,14 @@
+'use client'
+
 import posthog from 'posthog-js'
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && !window.posthog) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
     person_profiles: 'identified_only',
     defaults: '2025-05-24',
     loaded: (ph) => {
+      console.log("✅ PostHog loaded early")
       window.posthog = ph
     },
   })
