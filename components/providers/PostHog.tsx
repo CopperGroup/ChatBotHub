003befcddc9eae7ba53f5b12291ef887fec8fba8
tymyevhenmpw <tymyevhen@gmail.com>
2025-07-22 21:31:36 +0200

    Commit

--- components/providers/PostHog.tsx
+++ components/providers/PostHog.tsx
@@ -9,11 +9,14 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+    console.log("Loading")
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-05-24',
-      loaded: (ph) => (window.posthog = ph)
+      loaded: (ph) => {
+        window.posthog = ph
+      },
     })
   }, [])
 
