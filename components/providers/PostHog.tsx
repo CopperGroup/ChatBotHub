36d69754f877d5c9a1d130d0ba2fbc5d01beb50d
tymyevhenmpw <tymyevhen@gmail.com>
2025-07-23 11:47:18 +0200

    Commit

--- components/providers/PostHog.tsx
+++ components/providers/PostHog.tsx
@@ -1,19 +1,9 @@
-// app/providers.tsx
 'use client'
 
-import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+import posthog from '@/lib/posthogClient'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-      console.log(window)
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        defaults: '2025-05-24',
-      })
-  }, [])
-
   return (
     <PHProvider client={posthog}>
       {children}
